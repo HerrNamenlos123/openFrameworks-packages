189659171529a078b239f84775281ce662602579
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-07-17 14:41:41 +0200

    Impl libcurl

--- packages/libcurl/build.py
+++ packages/libcurl/build.py
@@ -1,33 +1,45 @@
 from scripts import tools
 import os
 
-# tools.clone_git_repository(git_repository = "https://github.com/Mbed-TLS/mbedtls.git",
-#                            git_tag = "v3.4.0",
-#                            working_dir = "mbedtls")
+# tools.clear_temp_folder()
+# tools.clone_git_repository(package_name = "mbedtls",
+#                            git_repository = "https://github.com/Mbed-TLS/mbedtls.git",
+#                            git_tag = "v3.4.0")
 
-# tools.build_generic_cmake_project(working_dir = "mbedtls",
+# tools.build_generic_cmake_project(package_name = "mbedtls",
 #                                   cmake_args = ["-DENABLE_TESTING=OFF",
 #                                                 "-DENABLE_PROGRAMS=OFF",
-#                                                 "-DBUILD_SHARED_LIBS=OFF",
 #                                                 "-DGEN_FILES=ON"])
 
-# tools.clone_git_repository(git_repository = "https://github.com/curl/curl.git",
+# tools.clone_git_repository(package_name = "libcurl",
+#                            git_repository = "https://github.com/curl/curl.git",
 #                            git_tag = "curl-8_1_2")
 
-# tools.remove_file(file = tools.WORKING_DIR + tools.SOURCE_DIR + "/CMake/FindMbedTLS.cmake") # Forcefully remove curl's FindMbedTLS.cmake to make it find ours instead
+# # Forcefully remove curl's FindMbedTLS.cmake to make it find ours instead
+# tools.remove_file(file = os.sep.join([tools.get_source_dir('libcurl'), "/CMake/FindMbedTLS.cmake"]))
 
-# tools.replace_in_file(file = tools.WORKING_DIR + tools.SOURCE_DIR + "/CMakeLists.txt",
-#                       find = "${MBEDTLS_LIBRARIES}", replace = "MbedTLS::mbedtls")  # Use our targets
+# tools.replace_in_file(file = os.sep.join([tools.get_source_dir('libcurl'), "/CMakeLists.txt"]),
+#                       find = "${MBEDTLS_LIBRARIES}", replace = "MbedTLS::mbedtls")  # Use our modern targets instead of legacy approach
 
-# tools.build_generic_cmake_project(cmake_args = ["-DBUILD_SHARED_LIBS=OFF", 
-#                                                 "-DBUILD_CURL_EXE=OFF",
-#                                                 "-DENABLE_UNICODE=ON",
-#                                                 "-DCURL_USE_MBEDTLS=ON"],
-#                                   cmake_args_debug = [f'-DMbedTLS_DIR="{os.getcwd()}/mbedtls{tools.INSTALL_DIR_DEBUG}/lib/cmake/MbedTLS"'],
-#                                   cmake_args_release = [f'-DMbedTLS_DIR="{os.getcwd()}/mbedtls{tools.INSTALL_DIR_RELEASE}/lib/cmake/MbedTLS"'])
+tools.build_generic_cmake_project(
+    package_name = "libcurl",
+    cmake_args_debug = [
+        f'-DMbedTLS_DIR="{os.sep.join([tools.get_debug_install_dir("mbedtls"), "lib", "cmake", "MbedTLS"])}"'
+    ],
+    cmake_args_release = [
+        f'-DMbedTLS_DIR="{os.sep.join([tools.get_release_install_dir("mbedtls"), "lib", "cmake", "MbedTLS"])}"'
+    ],
+    cmake_args = [
+        "-DBUILD_SHARED_LIBS=OFF", 
+        "-DBUILD_CURL_EXE=OFF",
+        "-DENABLE_UNICODE=ON",
+        "-DCURL_USE_MBEDTLS=ON"
+    ]
+)
 
-tools.archive_generic_package(files = [
-    [tools.WORKING_DIR + tools.INSTALL_DIR_DEBUG + "/include", "include"],
-    [tools.WORKING_DIR + tools.INSTALL_DIR_DEBUG + "/lib", "lib"],
-    [tools.WORKING_DIR + tools.INSTALL_DIR_RELEASE + "/lib", "lib"]
+tools.archive_generic_package(package_name = 'libcurl', files = [
+    [tools.get_debug_install_dir('mbedtls'), "debug"],
+    [tools.get_release_install_dir('mbedtls'), "release"],
+    [tools.get_debug_install_dir('libcurl'), "debug"],
+    [tools.get_release_install_dir('libcurl'), "release"],
 ])
--- scripts/tools.py
+++ scripts/tools.py
@@ -2,33 +2,36 @@ import os
 import sys
 import shutil
 
-ROOT = os.getcwd()
-WORKING_DIR = 'temp'
-SOURCE_DIR = '/source'
-BUILD_DIR_DEBUG = '/build-debug'
-BUILD_DIR_RELEASE = '/build-release'
-INSTALL_DIR_DEBUG = '/install-debug'
-INSTALL_DIR_RELEASE = '/install-release'
-OUTPUT_DIR = '/out'
+def get_root_dir():
+    return os.getcwd()
+
+def get_working_dir(package_name):
+    return os.sep.join([get_root_dir(), 'temp', package_name])
+
+def get_source_dir(package_name):
+    return os.sep.join([get_working_dir(package_name), 'source'])
+
+def get_debug_build_dir(package_name):
+    return os.sep.join([get_working_dir(package_name), 'build_debug'])
+
+def get_release_build_dir(package_name):
+    return os.sep.join([get_working_dir(package_name), 'build_release'])
+
+def get_debug_install_dir(package_name):
+    return os.sep.join([get_working_dir(package_name), 'install_debug'])
+
+def get_release_install_dir(package_name):
+    return os.sep.join([get_working_dir(package_name), 'install_release'])
+
+def get_archive_dir(package_name):
+    return os.sep.join([get_working_dir(package_name), 'archive'])
+
+def get_output_dir():
+    return os.sep.join([get_root_dir(), 'out'])
 
 def cmd(command):
     if os.system(command) != 0:
         raise Exception(f'Failed to execute command: {command}')
-    
-def debug_lib_name(name, is_debug = False):
-    if (os.environ['CXX_COMPILER'] == 'msvc'):
-        return f'{name}d.lib'
-    else:
-        return f'lib{name}d.a'
-    
-def release_lib_name(name, is_debug = False):
-    if (os.environ['CXX_COMPILER'] == 'msvc'):
-        return f'{name}.lib'
-    else:
-        return f'lib{name}.a'
-    
-def native_pdb_name(name):
-    return f'{name}.pdb'
 
 def replace_in_file(file, find, replace):
     with open(file, 'r') as f:
@@ -47,12 +50,11 @@ def insert_before_file(file, insert):
     with open(file, 'w') as f:
         f.write(content)
 
-def clone_git_repository(git_repository, git_tag, working_dir = WORKING_DIR):
-    if (os.path.exists(working_dir)):
-        shutil.rmtree(working_dir)
+def clone_git_repository(git_repository, git_tag, package_name):
+    sourcedir = get_source_dir(package_name)
 
     cmd('git config --global advice.detachedHead false')
-    cmd(f'git clone {git_repository} {working_dir + SOURCE_DIR} --depth=1 --single-branch --branch={git_tag}')
+    cmd(f'git clone {git_repository} {sourcedir} --depth=1 --single-branch --branch={git_tag}')
 
 def install_build_requirements(extra_unix_dependencies = []):
     if (os.environ['CXX_COMPILER'] != 'msvc'):
@@ -64,7 +66,7 @@ def install_build_requirements(extra_unix_dependencies = []):
     deps = ' '.join(extra_unix_dependencies)
     cmd(f'apt-get update && apt-get install -y {deps}')
 
-def build_generic_cmake_project(working_dir = WORKING_DIR, cmake_args = [], cmake_module_paths = [], cmake_module_path_root = '', cmake_args_debug = [], cmake_args_release = []):
+def build_generic_cmake_project(package_name, cmake_args = [], cmake_module_paths = [], cmake_module_path_root = '', cmake_args_debug = [], cmake_args_release = []):
     if (os.environ['CXX_COMPILER'] != 'msvc'):
         cmake_args.append(f'-DCMAKE_C_COMPILER={os.environ["CC_COMPILER"]}')
     
@@ -78,80 +80,59 @@ def build_generic_cmake_project(working_dir = WORKING_DIR, cmake_args = [], cmak
     cmake_args.append(f'-DPython_ROOT_DIR={os.path.dirname(sys.executable)}')
     cmake_args.append(f'-DPython3_ROOT_DIR={os.path.dirname(sys.executable)}')
 
-    os.makedirs(working_dir + BUILD_DIR_DEBUG, exist_ok=True)
-    os.makedirs(working_dir + BUILD_DIR_RELEASE, exist_ok=True)
+    if cmake_module_paths != []:
+        cmake_args.append(f'-DCMAKE_MODULE_PATH={";".join(cmake_module_paths)}')
 
-    args_debug = f'{working_dir + SOURCE_DIR}'
-    args_debug += f' -B {working_dir + BUILD_DIR_DEBUG}'
-    modpath_debug = f';{cmake_module_path_root + INSTALL_DIR_DEBUG}' if cmake_module_path_root != '' else ''
-    if cmake_module_paths != [] and modpath_debug == '':
-        args_debug += f' -DCMAKE_MODULE_PATH={";".join(cmake_module_paths)}{modpath_debug}'
-    args_debug += f' -DCMAKE_BUILD_TYPE=Debug'
-    args_debug += f' -DCMAKE_INSTALL_PREFIX={working_dir + INSTALL_DIR_DEBUG}'
-    args_debug += f' {" ".join(cmake_args)}'
-    args_debug += f' {" ".join(cmake_args_debug)}'
+    os.makedirs(get_debug_build_dir(package_name), exist_ok=True)
+    os.makedirs(get_release_build_dir(package_name), exist_ok=True)
 
-    args_release = f'{working_dir + SOURCE_DIR}'
-    args_release += f' -B {working_dir + BUILD_DIR_RELEASE}'
-    modpath_release = f';{cmake_module_path_root + INSTALL_DIR_RELEASE}' if cmake_module_path_root != '' else ''
-    if cmake_module_paths != [] and modpath_release == '':
-        args_release += f' -DCMAKE_MODULE_PATH={";".join(cmake_module_paths)}{modpath_release}'
-    args_release += f' -DCMAKE_BUILD_TYPE=Release'
-    args_release += f' -DCMAKE_INSTALL_PREFIX={working_dir + INSTALL_DIR_RELEASE}'
-    args_release += f' {" ".join(cmake_args)}'
-    args_release += f' {" ".join(cmake_args_release)}'
+    args_debug = []
+    args_debug.append(f'{get_source_dir(package_name)}')
+    args_debug.append(f'-B {get_debug_build_dir(package_name)}')
+    args_debug.append(f'-DCMAKE_BUILD_TYPE=Debug')
+    args_debug.append(f'-DCMAKE_INSTALL_PREFIX={get_debug_install_dir(package_name)}')
+    args_debug.append(' '.join(cmake_args))
+    args_debug.append(' '.join(cmake_args_debug))
 
-    cmd(f'cmake -G Ninja {args_debug}')
-    cmd(f'cmake --build {working_dir + BUILD_DIR_DEBUG} { "-j $(nproc)}" if os.environ["CXX_COMPILER"] != "msvc" else "" }')
-    cmd(f'cmake --build {working_dir + BUILD_DIR_DEBUG} --target install')
+    args_release = []
+    args_release.append(f'{get_source_dir(package_name)}')
+    args_release.append(f'-B {get_release_build_dir(package_name)}')
+    args_release.append(f'-DCMAKE_BUILD_TYPE=Release')
+    args_release.append(f'-DCMAKE_INSTALL_PREFIX={get_release_install_dir(package_name)}')
+    args_release.append(' '.join(cmake_args))
+    args_release.append(' '.join(cmake_args_release))
 
-    cmd(f'cmake -G Ninja {args_release}')
-    cmd(f'cmake --build {working_dir + BUILD_DIR_RELEASE} { "-j $(nproc)}" if os.environ["CXX_COMPILER"] != "msvc" else "" }')
-    cmd(f'cmake --build {working_dir + BUILD_DIR_RELEASE} --target install')
+    cmd(f'cmake -G Ninja {" ".join(args_debug)}')
+    cmd(f'cmake --build {get_debug_build_dir(package_name)} --config Debug { "-j $(nproc)}" if os.environ["CXX_COMPILER"] != "msvc" else "" }')
+    cmd(f'cmake --build {get_debug_build_dir(package_name)} --target install')
 
-def rename_debug_libfile(name, 
-                         install_dir_debug = WORKING_DIR + INSTALL_DIR_DEBUG, 
-                         install_dir_release = WORKING_DIR + INSTALL_DIR_RELEASE):
-    if os.path.exists(install_dir_debug + f'/lib/{name}d.lib'):
-        os.rename(install_dir_debug + f'/lib/{name}d.lib', install_dir_debug + f'/lib/{name}.lib')
+    cmd(f'cmake -G Ninja {" ".join(args_release)}')
+    cmd(f'cmake --build {get_release_build_dir(package_name)} --config Release { "-j $(nproc)}" if os.environ["CXX_COMPILER"] != "msvc" else "" }')
+    cmd(f'cmake --build {get_release_build_dir(package_name)} --target install')
 
-    if os.path.exists(install_dir_release + f'/lib/{name}d.lib'):
-        os.rename(install_dir_release + f'/lib/{name}d.lib', install_dir_release + f'/lib/{name}.lib')
-
-# def archive_generic_package(files,
-#                             cmake_file = '/repo/cmake/generic/import.cmake'):
-#     os.makedirs(WORKING_DIR + '/archive/debug/lib', exist_ok=True)
-#     os.makedirs(WORKING_DIR + '/archive/release/lib', exist_ok=True)
-#     os.makedirs('/package/out', exist_ok=True)
-
-#     shutil.copy(cmake_file, WORKING_DIR + '/archive/')
-#     shutil.copy(INSTALL_DIR_DEBUG + f'/lib/{native_lib_name(output_library_name)}', WORKING_DIR + '/archive/debug/lib/')
-#     shutil.copy(INSTALL_DIR_RELEASE + f'/lib/{native_lib_name(output_library_name)}', WORKING_DIR + '/archive/release/lib/')
-
-#     if os.path.exists(INSTALL_DIR_DEBUG + f'/lib/{native_pdb_name(output_library_name)}'):
-#         shutil.copy(INSTALL_DIR_DEBUG + f'/lib/{native_pdb_name(output_library_name)}', WORKING_DIR + '/archive/debug/lib/')
-
-#     shutil.copytree(INSTALL_DIR_DEBUG + '/include', WORKING_DIR + '/archive/debug/include')
-#     shutil.copytree(INSTALL_DIR_RELEASE + '/include', WORKING_DIR + '/archive/release/include')
-
-#     cmd(f'cd {WORKING_DIR}/archive && tar -zcvf /package/out/{os.environ["FULL_PACKAGE_NAME"]}.tar.gz *')
-
-def archive_generic_package(files, working_dir = WORKING_DIR, output_dir = ROOT + OUTPUT_DIR, default_cmake_file = True):
-    os.makedirs(working_dir + '/archive', exist_ok=True)
-    os.makedirs(output_dir, exist_ok=True)
-
-    if default_cmake_file:
-        shutil.copy('cmake/generic/import.cmake', working_dir + '/archive/')
+def archive_generic_package(package_name, files):
+    os.makedirs(get_archive_dir(package_name), exist_ok=True)
+    os.makedirs(get_output_dir(), exist_ok=True)
 
     for file in files:
-        sourcefile = file[0]
-        targetfile = working_dir + '/archive/' + file[1]
+        sourcefile = ''
+        targetfile = ''
 
-        os.makedirs(os.path.dirname(targetfile), exist_ok=True)
-        
+        if (len(file) == 2):
+            sourcefile = file[0]
+            targetfile = os.sep.join([get_archive_dir(package_name), file[1]])
+        else:
+            sourcefile = os.sep.join([file[0], file[1]])
+            targetfile = os.sep.join([get_archive_dir(package_name), file[2]])
+
         if os.path.isdir(sourcefile):
             shutil.copytree(sourcefile, targetfile, dirs_exist_ok=True)
         else:
+            os.makedirs(os.path.dirname(targetfile), exist_ok=True)
             shutil.copy(sourcefile, targetfile)
-    print(f'{output_dir}/{os.environ["FULL_PACKAGE_NAME"]}.tar.gz')
-    cmd(f'cd {working_dir}/archive && tar -zcvf {output_dir}/{os.environ["FULL_PACKAGE_NAME"]}.tar.gz *')
+
+    cmd(f'cd {get_archive_dir(package_name)} && tar -zcvf {get_output_dir()}/{os.environ["FULL_PACKAGE_NAME"]}.tar.gz *')
+
+def clear_temp_folder():
+    if os.path.exists(os.sep.join([get_root_dir(), 'temp'])):
+        shutil.rmtree(os.sep.join([get_root_dir(), 'temp']))
